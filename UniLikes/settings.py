f0ef942a2db31b1f3a74ceed3e79143af18f9df1
isis <izidacasillas@mail.ru>
2020-01-30 13:03:26 +0300

    connected PostgreSQL and added models

--- UniLikes/settings.py
+++ UniLikes/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'UniLikes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'unilikes',
+        'USER': 'postgres',
+        'PASSWORD': 'naxisucks',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- firstapp/models.py
+++ firstapp/models.py
@@ -2,6 +2,21 @@ from django.db import models
 
 
 class Teacher(models.Model):
-    name = models.CharField(max_length=30, verbose_name='Teacher')
+    surname = models.CharField(max_length=30, verbose_name='Surname')
+    name = models.CharField(max_length=30, verbose_name='Name')
+    second_name = models.CharField(max_length=30, verbose_name='Second name')
     rating = models.FloatField(default=0, verbose_name='Rating')
-    url = models.URLField(verbose_name='KFU account')
+    photo_url = models.URLField(verbose_name='Photo')
+
+    def __str__(self):
+        return self.surname + ' ' + self.name + ' ' + self.second_name
+
+
+class Subject(models.Model):
+    title = models.CharField(max_length=30)
+
+
+class CourseSubjectTeacher(models.Model):
+    course = models.IntegerField()
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
