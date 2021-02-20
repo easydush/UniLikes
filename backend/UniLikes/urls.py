c525753ef6845dfefa7828083417bf99804afa62
easydush <easydush@gmail.com>
2021-02-20 11:42:38 +0300

    created React app

--- backend/UniLikes/urls.py
+++ backend/UniLikes/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from rest_framework import permissions
-from django.urls import path, include, re_path
+from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -34,9 +33,9 @@ schema_view = get_schema_view(
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path to djoser end points
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
+    # path('api/', include(router.urls)),
+    path('api/auth/', include('djoser.urls')),
+    path('api/auth/', include('djoser.urls.authtoken')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     # path to our account's app endpoints
--- backend/voting/models.py
+++ backend/voting/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-from backend.account.models import Teacher, User
+from account.models import Teacher, User
 
 
 class RateFact(models.Model):
