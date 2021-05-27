1effdc53934ae22c23888d2b517395de6d69a3bf
easydush <easydush@gmail.com>
2021-05-28 00:02:54 +0300

    added some tests and docstrings somewhere

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -13,16 +13,29 @@ from rest_framework.test import APIClient
 
 @pytest.fixture
 def user():
+    """
+    Factory that returns random named user.
+    :return: User
+    """
     return UserFactory()
 
 
 @pytest.fixture
 def teacher():
+    """
+    Factory that returns random named teacher.
+    :return: Teacher
+    """
     return TeacherFactory()
 
 
 @pytest.fixture
 def authenticated_user(user):
+    """
+    Factory that returns authenticated user.
+    :param user: existing user.
+    :return: User
+    """
     api_client = APIClient()
     api_client.force_authenticate(user=user)
     return api_client
@@ -30,6 +43,9 @@ def authenticated_user(user):
 
 @pytest.fixture(scope='session')
 def django_db_setup():
+    """
+    Fixture for setting database that will be used in tests.
+    """
     settings.DATABASES['default'] = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': env('DB_NAME'),
--- backend/tests/test_voting/test_views.py
+++ backend/tests/test_voting/test_views.py
@@ -6,10 +6,30 @@ from teacher.models import Teacher
 
 
 @pytest.mark.django_db
-def test_voting(authenticated_user):
+def test_voting_counts_incrementing(authenticated_user):
+    """
+    Testing method to vote and check that teacher's votes count is changing.
+    :param authenticated_user: user authorized by factory (look for the FactoryBoy)
+    """
     teacher = Teacher.objects.last()
     rate = random.choice([0, 1])
     response = authenticated_user.post('http://localhost:8000/api/voting/', {'teacher_id': teacher.id, 'rate': rate},
                                        format='json')
     updated_teacher = Teacher.objects.last()
     assert teacher.vote_counts != updated_teacher.vote_counts
+
+
+@pytest.mark.django_db
+def test_voting_rating_changing(authenticated_user):
+    """
+Testing method to vote and check that teacher's votes rating is changing.
+    :param authenticated_user: user authorized by factory (look for the FactoryBoy)
+    """
+    teacher = Teacher.objects.last()
+    for i in range(3):
+        rate = i % 2
+        response = authenticated_user.post('http://localhost:8000/api/voting/',
+                                           {'teacher_id': teacher.id, 'rate': rate},
+                                           format='json')
+    updated_teacher = Teacher.objects.last()
+    assert teacher.rating != updated_teacher.rating
